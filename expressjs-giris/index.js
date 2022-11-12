1ea484b424b3519b1778b2dba64dcfbb8c20e0a3
Samed Kütahyalı <smdkthyli@gmail.com>
2022-11-12 11:28:53 +0300

    first commit

---
+++ expressjs-giris/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const aktorlerRouter = require('./routers/aktorlerRouter');
+
+const server = express();
+server.use(express.json());
+server.use('/aktorler', aktorlerRouter);
+
+server.get("/", (req, res) => {
+    res.send("Express'ten merhaba.")
+});
+
+
+
+server.listen(5000, () => {
+    console.log('http://localhost:5000 adresine gelen istekler dinleniyor.');
+})
